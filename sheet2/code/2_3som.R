22459f88f15f24e2ae3e4b392e5d5019416a80bd
Leonard Kleinhans <leonard.kleinhans42@gmail.com>
2014-11-12 12:14:37 +0100

    code refactoring for submission

--- sheet2/code/2_3som.R
+++ sheet2/code/2_3som.R
@@ -5,32 +5,21 @@ randomData <- function() {
   return(m)
 }
 
-trainsom <- function(data, iterations) {
+trainsom <- function(data, iterations, rad) {
   plot(0:27,0:27,type="n")
 
-  map5 <- som(data, 10, 10, init="random", rlen=iterations, radius=5)
-  map3 <- som(data, 10, 10, init="random", rlen=iterations, radius=3)
-  map1 <- som(data, 10, 10, init="random", rlen=iterations, radius=1)
+  map <- som(data, 10, 10, init="random", rlen=iterations, radius=rad)
   
   dim(data)<-c(10,10,4)
   text(2,11,"input data")
   rasterImage(data, xleft=0, xright=10, ybottom=0, ytop=10)
   
-  codevectors5<-map5[["code"]]
-  codevectors3<-map3[["code"]]
-  codevectors1<-map1[["code"]]
-  dim(codevectors5)<-c(10,10,4)
-  dim(codevectors3)<-c(10,10,4)
-  dim(codevectors1)<-c(10,10,4)
-  codevectors5<-unclass(codevectors5)
-  codevectors3<-unclass(codevectors3)
-  codevectors1<-unclass(codevectors1)
-  text(2,26,"radius=5")
-  rasterImage(codevectors5, xleft=0, xright=10, ybottom=15, ytop=25)
-  text(17,26,"radius=3")
-  rasterImage(codevectors3, xleft=15, xright=25, ybottom=15, ytop=25)
-  text(17,11,"radius=1")
-  rasterImage(codevectors1, xleft=15, xright=25, ybottom=0, ytop=10)
+  codevectors<-map[["code"]]
   
+  dim(codevectors)<-c(10,10,4)
   
+  codevectors<-unclass(codevectors)
+  
+  text(2,26,"code vectors")
+  rasterImage(codevectors, xleft=0, xright=10, ybottom=15, ytop=25)
 }
