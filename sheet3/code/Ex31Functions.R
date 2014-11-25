66fafcf0f89f607edcba7b74ae7dd92525456d18
Xst4r <danny-heinrich@gmx.de>
2014-11-25 17:00:40 +0100

    added plots for the three sets

--- sheet3/code/Ex31Functions.R
+++ sheet3/code/Ex31Functions.R
@@ -1,5 +1,5 @@
 news <- function(x){
- -((x-500)*(x-5000))/(4*1265625)
+  -((x-500)*(x-5000))/(4*1265625)
 }
 poem <- function(x){
   return (-((x-0)*(x-1000))/(4*62500))
@@ -8,7 +8,7 @@ book <- function(x){
   if(x >150000) return(1)
   if(x <50000) return(0)
   else
-  return((x/100000)-0.5)
+    return((x/100000)-0.5)
 }
 
 child <- function(x){
@@ -74,19 +74,47 @@ crowd <- function(x){
 
 
 
-plotFuzzy<-function(){
-  x <- seq(from=0,to=200000)
-  y <- sapply(x,book)
-  plot(x,y,type="l")
-  x <- seq(from=500,to=5000)
-  y <- sapply(x,news)
-  dev.new()
-  plot(x,y,type="l")
-  x <- seq(from=0,to=1000)
-  y <- sapply(x,poem)
-  dev.new()
-  plot(x,y,type="l",ylim=c(0,1))
-
+plotPeople<-function(){
+  x <- seq(from=0,to=100)
+  y <- sapply(x,group)
+  plot(x,y,type="l",xlab="Number of People",col="red")
+  x <- seq(from=0,to=100)
+  y <- sapply(x,class)
+  points(x,y,type="l",col="blue")
+  x <- seq(from=0,to=100)
+  y <- sapply(x,crowd)
+  points(x,y,type="l",ylim=c(0,1),col="green",xlab="crowd")
+  
   
 }
 
+plotWords<-function(){
+  x <- seq(from=0,to=160000)
+  y <- sapply(x,book)
+  plot(x,y,type="l",xlab="Number of Words",col="red")
+  x <- seq(from=500,to=5000)
+  y <- sapply(x,news)
+  points(x,y,type="l",col="blue")
+  x <- seq(from=0,to=1000)
+  y <- sapply(x,poem)
+  points(x,y,type="l",ylim=c(0,1),col="green",xlab="crowd")
+  
+  
+}
+
+plotAge<-function(){
+  x <- seq(from=0,to=100)
+  y <- sapply(x,child)
+  plot(x,y,type="l",ylim=c(0,2),xlab="Age (Red=Child, Green=YoungPerson, Blue=GrownUp, Purple=OldPerson)",col="red")
+  x <- seq(from=0,to=100)
+  y <- sapply(x,grownup)
+  points(x,y,type="l",col="blue")
+  x <- seq(from=0,to=100)
+  y <- sapply(x,youngperson)
+  points(x,y,type="l",ylim=c(0,2),col="green",xlab="crowd")
+  x <- seq(from=0,to=100)
+  y <- sapply(x,oldperson)
+  points(x,y,type="l",ylim=c(0,2),col="purple",xlab="crowd")
+  
+  
+}
